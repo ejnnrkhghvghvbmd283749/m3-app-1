97c64a411e2a321038ffbe0eb3a8c67fe215e22d
elnaz.imani@student.lut.fi <elnaz.imani@student.lut.fi>
2025-03-30 14:18:48 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,6 +2,7 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import datetime
+import time
 
 NUMBER_TO_MONTH = {
   1: 'January',
@@ -22,8 +23,16 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.month = datetime.datetime.now().month
+    self.now = time.localtime()
+    
     self.init_components(**properties)
+    
     self.label_1.text = self.month_name()
+    self.label_2.text = self.week_number()
     # Any code you write here will run before the form opens.    
   def month_name(self):
     return NUMBER_TO_MONTH[self.month]
+    
+  def week_number(self):
+    week_num = int(time.strftime("%W")) + 1 
+    return "Week " + str(week_num)
