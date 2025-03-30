3b767ebbf6e722655a5595c702bcd19f12169aa6
elnaz.imani@student.lut.fi <elnaz.imani@student.lut.fi>
2025-03-30 12:56:02 +0000

    Created DayNames, ServerModule1 and Day; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,28 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
+NUMBER_TO_MONTH = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December',
+}
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.month = 11
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    self.label_1.txt = self.month_name()
+    # Any code you write here will run before the form opens.    
+  def month_name(self):
+    return NUMBER_TO_MONTH[self.month]
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,7 @@
+import anvil.server
+from calendar import Calendar
+
+@anvil.server.callable
+def get_days(month):
+  cal = Calendar()
+  return list(cal.itermonthdates(2018, month))
